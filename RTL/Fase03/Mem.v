4e2e004ee2dea94a89d4bfaf0258eff43c42ef23
Hackercio <jose.alvarez8749@alumnos.udg.mx>
2026-05-23 18:58:42 -0600

    Update Mem.v

--- RTL/Fase03/Mem.v
+++ RTL/Fase03/Mem.v
@@ -14,11 +14,52 @@ module Mem(
 
 reg [31:0] mem[0:255];
 
+initial begin
+
+	// MATRIZ A
+	mem[0] = 1;
+	mem[1] = 2;
+	mem[2] = 3;
+
+	mem[3] = 4;
+	mem[4] = 5;
+	mem[5] = 6;
+
+	mem[6] = 7;
+	mem[7] = 8;
+	mem[8] = 9;
+
+	// MATRIZ B
+	mem[9]  = 9;
+	mem[10] = 8;
+	mem[11] = 7;
+
+	mem[12] = 6;
+	mem[13] = 5;
+	mem[14] = 4;
+
+	mem[15] = 3;
+	mem[16] = 2;
+	mem[17] = 1;
+
+	// MATRIZ C
+	mem[18] = 0;
+	mem[19] = 0;
+	mem[20] = 0;
+	mem[21] = 0;
+	mem[22] = 0;
+	mem[23] = 0;
+	mem[24] = 0;
+	mem[25] = 0;
+	mem[26] = 0;
+
+end
+
 // WRITE
 always @(posedge clk) begin
 
 	if(MemToWrite)
-		mem[address] <= dataWrite;
+		mem[address >> 2] <= dataWrite;
 
 end
 
@@ -26,7 +67,7 @@ end
 always @* begin
 
 	if(MemToRead)
-		readData = mem[address];
+		readData = mem[address >> 2];
 	else
 		readData = 32'd0;
 
